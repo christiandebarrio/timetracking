45647d6c53ce32ad2304d153ad797b2338ae4822
Christian de Barrio Arribas <christiandebarrio@gmail.com>
2015-11-17 10:27:29 +0100

    no_project_found view in projectsController

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,7 +2,7 @@ class ProjectsController < ApplicationController
   def index
     @projects = Project.last_created_projects(10)
       if @projects.empty?
-        render 'index', layout: 'no_projects'
+        render 'no_projects_found'
       end
   end
 
