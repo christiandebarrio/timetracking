8225cd101dc2c423baf8f4f71554018cf0d74ccd
Christian de Barrio Arribas <christiandebarrio@gmail.com>
2015-11-18 15:34:45 +0100

    validation form

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -6,4 +6,28 @@ class EntriesController < ApplicationController
     @entries = @project.entries.where(date: date.beginning_of_month..date.end_of_month)
     @total_hours = @project.total_hours_in_month(date.month, date.year)
   end
+
+  def new
+    @project = Project.find(params[:project_id])
+    @entry = @project.entries.new #new entry with project_id
+  end
+
+  def create
+    @project = Project.find(params[:project_id])
+    @entry = @project.entries.new(entry_params)
+
+    if @entry.save
+      redirect_to action: 'index', controller: 'entries', project_id: @project.id
+    else      
+      render 'new'
+    end
+
+  end
+
+  private
+
+  def entry_params
+    params.require(:entry).permit(:hours, :minutes, :date)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,14 @@ Rails.application.routes.draw do
   get '/' => 'site#home'
   get '/contact' => 'site#contact'
 
-  get '/projects' => 'projects#index'
-  get '/projects/:id' => 'projects#show'
-  get '/projects/:project_id/entries' => 'entries#index'
+  resources :projects, only: [:index, :show] do
+    resources :entries, only: [:index, :new, :create]
+  end
+
+  # get '/projects' => 'projects#index'
+  # get '/projects/:id' => 'projects#show'
+  # get '/projects/:project_id/entries' => 'entries#index'
+  # get '/projects/:project_id/entries/new' => 'entries#new'
 
 end
   
